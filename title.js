853f936eb84a9cf09f979155db5da533b3378a73
mulucy <muyucel2011@gmail.com>
2024-07-12 01:11:00 +0300

    Update title.js

--- title.js
+++ title.js
@@ -1,1 +1,10 @@
+var baslik = document.title;
+var yeniBaslik = "Beni Unutma, Unutursan Kahrolurum.";
+window.onblur = function(){
+  document.title = yeniBaslik;
+  }
+    
+window.onfocus = function() {
 
+ document.title = baslik;
+}
