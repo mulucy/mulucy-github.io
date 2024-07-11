4afb9e4d5f45e13b09377c2f258d3bab5e66b192
mulucy <muyucel2011@gmail.com>
2024-07-12 01:20:41 +0300

    Update title.js

--- title.js
+++ title.js
@@ -1,15 +1,15 @@
 var baslik = document.title;
 var yeniBaslik = "Beni Unutma, Unutursan Kahrolurum.";
-var icon = "assets/icons/mulucy.ico";
-var yeniIcon = "assets/icons/sad.ico";
+//var icon = "assets/icons/mulucy.ico";
+//var yeniIcon = "assets/icons/sad.ico";
 window.onblur = function(){
   document.title = yeniBaslik;
-  document.getElementByID("icon").href = yeniIcon;
+  //document.getElementByID("icon").href = yeniIcon;
   
   }
     
 window.onfocus = function() {
 
  document.title = baslik;
-document.getElementByID("icon").href = icon;
+//document.getElementByID("icon").href = icon;
 }
